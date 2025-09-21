c0be294f42558598616cad761b11ed853e32870f
mwm-3 <105498745+mwm-3@users.noreply.github.com>
2025-09-21 09:56:47 -0400

    Create rocks.js

---
+++ rocks.js
@@ -0,0 +1,25 @@
+const rocks = {
+  pipeline: [
+    "Mark spends X% more on Producer Activities",
+    "Chris is on track to $388K",
+    "Packaging plan executed",
+    "Josh shifts to LC prospecting",
+    "Know Packaging: Win big through NACD",
+    "Leverage our partners like: JOB, Alicia"
+  ],
+  leads: [
+    "We’re email marketing alumni successfully",
+    "Podcast doubles audience to 200 downloads/episode",
+    "Podcast w/Team First, then Guests",
+    "We’ve gotten 3 sales",
+    "We’re seeing 30+ Leads/Qtr",
+    "We’ve lit the 'We’re hiring' sign"
+  ],
+  renewals: [
+    "Commitment Monday and Friday of renewal Dashboard",
+    "Improved BTN collection and WIIFM",
+    "Increase LL submissions",
+    "Better proposals renewal template",
+    "No more Mr. Nice guy plan for adding people and being paid"
+  ]
+};
