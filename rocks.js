e186dbd795ae82d8e60155ce8fe291899dca1f4e
mwm-3 <105498745+mwm-3@users.noreply.github.com>
2025-09-21 10:15:49 -0400

    Update rocks.js
    with commas

--- rocks.js
+++ rocks.js
@@ -1,37 +1,36 @@
 const rocks = {
   pipeline: [
     "Mark spends X% more on Producer Activities",
-    "DS Ramp Up"
-    "Darryl sells $50k and heads to deposit $250K"
-    "Darryl Trains 6 workshops"
-    "Darry networks w/CEO groups"
-    "Chris deposits $60K by end of Q1"
-    "Cookbook dedication & HS reporting"
-    "Josh $300k deposits, 2.5 pipeline"
-    "Mark Closes >$500K+ of business"
-    "Investigate referral program with Darryl"
+    "DS Ramp Up",
+    "Darryl sells $50k and heads to deposit $250K",
+    "Darryl Trains 6 workshops",
+    "Darry networks w/CEO groups",
+    "Chris deposits $60K by end of Q1",
+    "Cookbook dedication & HS reporting",
+    "Josh $300k deposits, 2.5 pipeline",
+    "Mark Closes >$500K+ of business",
+    "Investigate referral program with Darryl",
     "CSM involvement for assumed close 2026 planning"
-
   ],
   leads: [
-    ">30 Leads for QTR"
-    "Enrich invalid and old email addresses for all Alumni"
-    "Define targeted accounts (JP)"
-    "Conduct Storytelling webinar (MM)"
-    "Market SKOs in Q4"
-    "Build podcast attendance"
-    "Segmented by Industry and Management y/n for targeted emails"
-    "Testimonials (videos, quotes)"
-    "Rock the NACD"
+    ">30 Leads for QTR",
+    "Enrich invalid and old email addresses for all Alumni",
+    "Define targeted accounts (JP)",
+    "Conduct Storytelling webinar (MM)",
+    "Market SKOs in Q4",
+    "Build podcast attendance",
+    "Segmented by Industry and Management y/n for targeted emails",
+    "Testimonials (videos, quotes)",
+    "Rock the NACD",
     "We’ve lit the 'We’re hiring' sign"
   ],
   renewals: [
-    "Improve BTN and LL Submissions: 1) Client sees a benefit from completing LL; 2) BTN Improves from $65M (MM)"
-    "Everboarding:  1) Plan completed; 2) ID and fix 1 big thing (NH)"
-    "Success Call Re-Work: Tracking in Dashboards, better success (IR)"
-    "Management Involvement: 1) Summary emails; 2) Mgr Meetings at every 90D"
-    "Possible Sales gap survey process for what does the team need (like Jody)"
-    "2-3 year contracts"
+    "Improve BTN and LL Submissions: 1) Client sees a benefit from completing LL; 2) BTN Improves from $65M (MM)",
+    "Everboarding:  1) Plan completed; 2) ID and fix 1 big thing (NH)",
+    "Success Call Re-Work: Tracking in Dashboards, better success (IR)",
+    "Management Involvement: 1) Summary emails; 2) Mgr Meetings at every 90D",
+    "Possible Sales gap survey process for what does the team need (like Jody)",
+    "2-3 year contracts",
     "No more Mr. Nice guy plan for adding people and being paid"
   ]
 };
