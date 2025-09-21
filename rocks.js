ad0f93831e2abde819ec4890da91463d0eaea0d1
mwm-3 <105498745+mwm-3@users.noreply.github.com>
2025-09-21 10:06:24 -0400

    Update rocks.js

--- rocks.js
+++ rocks.js
@@ -1,25 +1,37 @@
 const rocks = {
   pipeline: [
     "Mark spends X% more on Producer Activities",
-    "Chris is on track to $388K",
-    "Packaging plan executed",
-    "Josh shifts to LC prospecting",
-    "Know Packaging: Win big through NACD",
-    "Leverage our partners like: JOB, Alicia"
+    "DS Ramp Up"
+    "Darryl sells $50k and heads to deposit $250K"
+    "Darryl Trains 6 workshops"
+    "Darry networks w/CEO groups"
+    "Chris deposits $60K by end of Q1"
+    "Cookbook dedication & HS reporting"
+    "Josh $300k deposits, 2.5 pipeline"
+    "Mark Closes >$500K+ of business"
+    "Investigate referral program with Darryl"
+    "CSM involvement for assumed close 2026 planning"
+
   ],
   leads: [
-    "We’re email marketing alumni successfully",
-    "Podcast doubles audience to 200 downloads/episode",
-    "Podcast w/Team First, then Guests",
-    "We’ve gotten 3 sales",
-    "We’re seeing 30+ Leads/Qtr",
+    ">30 Leads for QTR"
+    "Enrich invalid and old email addresses for all Alumni"
+    "Define targeted accounts (JP)"
+    "Conduct Storytelling webinar (MM)"
+    "Market SKOs in Q4"
+    "Build podcast attendance"
+    "Segmented by Industry and Management y/n for targeted emails"
+    "Testimonials (videos, quotes)"
+    "Rock the NACD"
     "We’ve lit the 'We’re hiring' sign"
   ],
   renewals: [
-    "Commitment Monday and Friday of renewal Dashboard",
-    "Improved BTN collection and WIIFM",
-    "Increase LL submissions",
-    "Better proposals renewal template",
+    "Improve BTN and LL Submissions: 1) Client sees a benefit from completing LL; 2) BTN Improves from $65M (MM)"
+    "Everboarding:  1) Plan completed; 2) ID and fix 1 big thing (NH)"
+    "Success Call Re-Work: Tracking in Dashboards, better success (IR)"
+    "Management Involvement: 1) Summary emails; 2) Mgr Meetings at every 90D"
+    "Possible Sales gap survey process for what does the team need (like Jody)"
+    "2-3 year contracts"
     "No more Mr. Nice guy plan for adding people and being paid"
   ]
 };
